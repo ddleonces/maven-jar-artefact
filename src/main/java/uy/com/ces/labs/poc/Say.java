905c7e2b51253f69903a16f2c80b8f1a6ed9ebff
ddleonces <ddleon@ces.com.uy>
2023-08-29 15:49:22 -0300

    Commit initial

---
+++ src/main/java/uy/com/ces/labs/poc/Say.java
@@ -0,0 +1,42 @@
+package uy.com.ces.labs.poc;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Say {
+
+	private static final String ERR_EMPTY_LANG = 
+			"You must provide a language";
+	private static final String ERR_INVALID_LANG = 
+			"There is no translation for greetings in language '%s'";
+
+	private static final Map<String, String> GREETINGS = new LinkedHashMap<>();
+
+	static {
+		GREETINGS.put("ES", "Hola %s");
+		GREETINGS.put("EN", "Hello %s");
+		GREETINGS.put("PT", "Salut %s");
+	}
+
+	/**
+	 * @param lang Lenguaje en el que se debe realizar el saludo
+	 * @param name Nombre de la persona a saludar
+	 * @return Saludo a name en lenguaje lang
+	 * @throws IllegalArgumentException si name es inválido
+	 */
+	public String hello(final String lang, final String name) {
+		if (lang == null || lang.isBlank()) {
+			throw new IllegalArgumentException(ERR_EMPTY_LANG);
+		}
+
+		final String language = lang.toUpperCase();
+
+		if (!GREETINGS.containsKey(language)) {
+			throw new IllegalArgumentException(
+					ERR_INVALID_LANG.formatted(language));
+		}
+
+		return GREETINGS.get(language).formatted(name);
+	}
+
+}
---
+++ src/test/java/uy/com/ces/labs/poc/SayTest.java
@@ -0,0 +1,57 @@
+package uy.com.ces.labs.poc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class SayTest {
+
+	Say say;
+
+	@BeforeEach
+	void beforeEach() {
+		say = new Say();
+	}
+
+	@ParameterizedTest
+	@DisplayName("Check translations")
+	@CsvSource({ "ES,Juan,Hola Juan", "EN,Mary,Hello Mary", "PT,Ramona,Salut Ramona" })
+	void esTest(String lang, String name, String expect) {
+		assertEquals(expect, say.hello(lang, name));
+	}
+
+	@Test
+	@DisplayName("Check null language")
+	void nullLanguage() {
+		try {
+			say.hello(null, "Elsa");
+		} catch (IllegalArgumentException e) {
+			assertEquals("You must provide a language", e.getMessage());
+		}
+	}
+
+	@Test
+	@DisplayName("Check empty language")
+	void emptyLanguage() {
+		try {
+			say.hello("", "Santíago");
+		} catch (IllegalArgumentException e) {
+			assertEquals("You must provide a language", e.getMessage());
+		}
+	}
+
+	@Test
+	@DisplayName("Check invalid language")
+	void invalidLanguage() {
+		try {
+			say.hello("DE", "Friedrich");
+		} catch (IllegalArgumentException e) {
+			assertEquals("There is no translation for greetings in language 'DE'", e.getMessage());
+		}
+	}
+
+}
